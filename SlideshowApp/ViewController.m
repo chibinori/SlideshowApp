4150aef62fcbca9d4dad0a9049068f4999bf4219
Noriaki Sakai <noriaki.sakai.develop@gmail.com>
2015-12-02 14:04:20 +0900

    before developing Lesson5 SlideshowApp

--- SlideshowApp/ViewController.m
+++ SlideshowApp/ViewController.m
@@ -8,7 +8,10 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController () {
+    UIImageView *aImageView;
+    NSInteger countNumber;
+}
 
 @end
 
@@ -17,11 +20,38 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    countNumber = 0;
+    [self setupBackground];
+    [self setupButton];
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+-(void)setupBackground{
+    aImageView = [[UIImageView alloc] initWithFrame:CGRectMake(0, 0, 320, 568)];
+    aImageView.image = [UIImage imageNamed:@"flower0.png"];
+    [self.view addSubview:aImageView];
+}
+
+-(void)setupButton{
+    UIButton *button = [UIButton buttonWithType:UIButtonTypeCustom];
+    button.frame = CGRectMake(0, 0, 120, 45);
+    button.center = CGPointMake(160, 450);
+    [button setTitle:@"Button" forState:UIControlStateNormal];
+    [button addTarget:self action:@selector(changeImageUsingIf:)
+     forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:button];
+}
+
+-(void)changeImageUsingIf:(id)sender{
+    countNumber++;
+    aImageView.image = [UIImage imageNamed:[NSString stringWithFormat:@"flower%ld.png",(long)countNumber]];
+    if (countNumber==3) {
+        countNumber = 0;
+    }
 }
 
 @end
